b713038515f1f92a40e05b5d4ca28eeafb96eb52
Muhammad Romli <genbugenba@gmail.com>
2026-08-10 22:02:31 +0700

    updating README and changing utility module to text_transformer for better description of the module

--- src/test_text_transformer.py
+++ src/test_text_transformer.py
@@ -1,5 +1,5 @@
 import unittest
-from utility import split_nodes_delimiter, text_to_textnodes, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from text_transformer import split_nodes_delimiter, text_to_textnodes, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextType, TextNode
 
 
