5ab6db4eb7119a8dc1bce3bac7ffdc46a3bda318
Muhammad Romli <genbugenba@gmail.com>
2026-08-14 20:42:52 +0700

    Adding Features: Now you can put your md files in directory 'content' see more in README

--- src/file_operator/set_up_html.py
+++ src/file_operator/set_up_html.py
@@ -10,8 +10,8 @@ def extract_title(markdown: str) -> str:
     raise Exception('make sure to put line with single "#"(h1) inside of your markdown')
 
 
-def generate_page(from_path:str, template_path:str, dest_path: str):
-    print(f"Generating page from {from_path} to {dest_path} using tesmplate {template_path}")
+def generate_page(from_path:str, template_path:str, dest_path: str, basepath: str):
+    print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, "r", encoding="utf-8") as file:
         markdown_content = file.read()
     with open(template_path, "r", encoding="utf-8") as template:
@@ -19,12 +19,28 @@ def generate_page(from_path:str, template_path:str, dest_path: str):
 
     html_content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
-    half_ready_html = template_content.replace("{{ Title }}", title)
-    ready_html = half_ready_html.replace("{{ Content }}", html_content)
+    half_ready_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    ready_html = half_ready_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
-    dest_path_parent = os.path.dirname(dest_path) #if dest_path is only file is gonna return ""
-    if dest_path_parent:
-        os.makedirs(dest_path_parent, exist_ok=True)
+    dest_dir_path = os.path.dirname(dest_path) #if dest_path is only file is gonna return ""
+    if dest_dir_path:
+        os.makedirs(dest_dir_path, exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as destination:
         destination.write(ready_html)
 
+ 
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path, basepath:str):
+
+    for content in os.listdir(dir_path_content):
+        joined_dir_content_path = os.path.join(dir_path_content, content)
+        if content.endswith(".md"):
+            no_md_extension = content[:-3]
+            html_extension_content = no_md_extension + ".html"
+            joined_dest_dir_path = os.path.join(dest_dir_path, html_extension_content)
+        else:
+            joined_dest_dir_path = os.path.join(dest_dir_path, content)
+
+        if os.path.isdir(joined_dir_content_path):
+            generate_pages_recursive(joined_dir_content_path, template_path, joined_dest_dir_path, basepath)
+        elif os.path.isfile(joined_dir_content_path):
+            generate_page(joined_dir_content_path, template_path, joined_dest_dir_path, basepath) # i use joined_dest_dir_path instead of dest_dir_path because generate_page need path of file like "content/images/hello.png", instead of the parent dir
--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from src.file_operator.file_operator import copy_filepath, remove_public
-from src.file_operator.set_up_html import generate_page
+from src.file_operator.set_up_html import generate_pages_recursive
+import sys
 
-
-
+if sys.argv[1]:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
 def main():
     y_or_n_question = input("You sure you want to proceed? this will remove all your file in public folder? (y/n)")
     if y_or_n_question == "y":
-        remove_public()
-        copy_filepath()
-        generate_page("content/index.md", "template.html", 'public/index.html')
-
+        generate_pages_recursive("content", "template.html", 'docs', basepath)
     elif y_or_n_question == "n":
         print("Operation Terminated")
         return
@@ -20,3 +20,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    
